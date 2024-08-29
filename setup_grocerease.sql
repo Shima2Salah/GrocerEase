f7dd73a2052251d76ae20895993735038d4876e4
Ahmed Adss <eng.ahmedads@gmail.com>
2024-08-29 04:10:10 -0700

    database setup and creating tables

--- setup_grocerease.sql
+++ setup_grocerease.sql
@@ -1,6 +1,5 @@
 -- prepares a  test SQL server for grocerease
 
-
 CREATE DATABASE IF NOT EXISTS grocerease;
 CREATE USER IF NOT EXISTS 'grocer_dev'@'localhost' IDENTIFIED BY 'grocer_dev_pwd';
 GRANT ALL PRIVILEGES ON `grocerease`.* TO 'grocer_dev'@'localhost';
