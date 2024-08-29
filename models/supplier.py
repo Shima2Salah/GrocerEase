23b339069b69b9bfcd8b16337efb4548cb66ed08
Shimaa Salah <shimaasalah91.13@gmail.com>
2024-08-29 12:36:28 +0300

    edit address property in supplier class

--- models/supplier.py
+++ models/supplier.py
@@ -5,6 +5,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 
+
 class Supplier(BaseModel, Base):
     """Representation of Supplier"""
     if models.storage_t == 'db':
@@ -12,17 +13,19 @@ class Supplier(BaseModel, Base):
         supplier_name = Column(String(255), nullable=False)
         contact_number = Column(String(50), nullable=False)
         address = Column(String(100), nullable=False)
-        created_by_admin_id = Column(Integer, ForeignKey('admins.id'), nullable=False)
+        created_by_admin_id = Column(Integer, ForeignKey('admins.id'),
+                                     nullable=False)
         company_name = Column(String(255), nullable=False)
         email = Column(String(100), unique=True, nullable=False)
         notes = Column(String(255))
         products = relationship(
             "Product",
-            backref="suppliers",  # Singular form since this is a many-to-one relationship
+            backref="suppliers",
             cascade="all, delete, delete-orphan"
         )
         # Relationship to Category
-        admin = relationship('Admin', back_populates='suppliers', overlaps="admin_relationship")
+        admin = relationship('Admin', back_populates='suppliers',
+                             overlaps="admin_relationship")
         products = relationship('Product', back_populates='supplier')
     else:
         supplier_name = ""
@@ -32,7 +35,6 @@ class Supplier(BaseModel, Base):
         company_name = ""
         email = ""
         notes = ""
-
 
     def __init__(self, *args, **kwargs):
         """Initializes Supplier"""
